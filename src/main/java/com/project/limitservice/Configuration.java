2e920414283fc754b0a0a5c97745e55777661d5d
Neetu <neetu.nandwani@gmail.com>
2020-08-27 19:21:17 +0530

    push

---
+++ src/main/java/com/project/limitservice/Configuration.java
@@ -0,0 +1,27 @@
+package com.project.limitservice;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "limit-service")
+@Component
+public class Configuration {
+    private int maximum;
+    private int minimum;
+
+    public int getMaximum() {
+        return maximum;
+    }
+
+    public void setMaximum(int maximum) {
+        this.maximum = maximum;
+    }
+
+    public int getMinimum() {
+        return minimum;
+    }
+
+    public void setMinimum(int minimum) {
+        this.minimum = minimum;
+    }
+}
